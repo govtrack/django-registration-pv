7bb9ba51d1672e134ca97d88e8f45108638a2474
GovTrack.us <operations@govtrack.us>
2012-12-18 10:07:40 -0500

    allow reassigning AuthRecords to other users inthe admin; add .gitignore file

--- registration/admin.py
+++ registration/admin.py
@@ -2,7 +2,8 @@ from models import *
 from django.contrib import admin
 
 class AuthRecordAdmin(admin.ModelAdmin):
-        readonly_fields = ("user", "uid", "provider")
+        readonly_fields = ("uid", "provider")
+        raw_id_fields = ('user',)
         search_fields = ["user__username", "user__email", "uid"]
         list_display = ["user", "provider", "uid"]
 
